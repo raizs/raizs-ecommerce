f953f4ec1e8f92b2a90c31f68266fb89a0e76487
morales <gabriel@raizs.com.br>
2019-06-19 17:23:06 -0300

    .

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import Loadable from 'react-loadable';
 
 import indexController from './controllers/index';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8081;
 
 // initialize the application and create the routes
 const app = express();
